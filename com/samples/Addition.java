c33f39a95ce19e718fbb9006dc806dfadd35e1ef
BollaVamshidar <vamshidar2002@gmail.com>
2024-11-03 11:54:43 +0530

    addition class

---
+++ com/samples/Addition.java
@@ -0,0 +1,13 @@
+package com.samples;
+import java.util.*;
+public class Addition{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a=sc.nextInt();
+        System.out.println("Enter second number");
+         int b=sc.nextInt();
+         int c=a+b;
+        System.out.println("result : "+c);
+    }
+}
